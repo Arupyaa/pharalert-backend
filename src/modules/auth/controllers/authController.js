ee5c3e25f465e96a0417e134fcaa22e7d0fb9c81
Arupyaa <amkeshk98@gmail.com>
2026-05-12 20:08:09 +0300

    added logout controller and service

--- src/modules/auth/controllers/authController.js
+++ src/modules/auth/controllers/authController.js
@@ -2,6 +2,7 @@ import { createEndUser } from "../services/registerService.js";
 import { endUserSchema } from "../validators/registerSchemas.js";
 import { loginSchema } from "../validators/loginSchema.js";
 import { loginService } from "../services/loginService.js";
+import { logoutService } from "../services/logoutService.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -60,6 +61,24 @@ export const loginUser = async (req, res) => {
             ...data,
         });
 
+    } catch (err) {
+        return res.status(err.status || 500).json({
+            message: err.message,
+        });
+    }
+};
+
+
+export const logoutUser = async (req, res) => {
+    try {
+        const refreshToken =
+            req.body.refreshToken || req.cookies?.refreshToken;
+
+        await logoutService(refreshToken);
+
+        return res.status(200).json({
+            message: "Logged out successfully",
+        });
     } catch (err) {
         return res.status(err.status || 500).json({
             message: err.message,
