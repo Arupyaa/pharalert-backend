213c847186c32e23415bd273dba64c2d3a264c39
Arupyaa <amkeshk98@gmail.com>
2026-05-12 16:21:02 +0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import pharmacyRoutes from "./modules/pharmacy/index.js"
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
