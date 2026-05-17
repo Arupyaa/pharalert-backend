ca7d8a4ccb299b6b4ee8787d745385c81cd983b9
Arupyaa <amkeshk98@gmail.com>
2026-05-17 16:07:34 +0300

    small bugfix for purchase controller and validator

--- src/modules/pharmacy/validators/getPurchasesSchema.js
+++ src/modules/pharmacy/validators/getPurchasesSchema.js
@@ -1,12 +1,10 @@
 import { z } from "zod";
 
 export const getPurchasesSchema = z.object({
-    query: z.object({
         page: z.coerce.number().int().positive().optional(),
         limit: z.coerce.number().int().positive().optional(),
         from: z.coerce.date().optional(),
         to: z.coerce.date().optional(),
         medicationId: z.coerce.number().optional(),
         order: z.enum(["asc", "desc"]).optional(),
-    }),
 });
